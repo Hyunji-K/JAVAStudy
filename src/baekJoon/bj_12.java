18ac51eb390a2cdd2942205dcc108855b1a7603a
HJ <kgj6200@naver.com>
2020-10-10 23:58:39 +0900

    1216

---
+++ src/baekJoon/bj_12.java
@@ -0,0 +1,14 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class bj_12 {
+ //2161번
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		// N장의 카드 입력받기
+		int n = scanner.nextInt(); 
+		//
+	}
+}
