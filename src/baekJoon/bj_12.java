c05f8557fedce437f6ec7b29a55bcb98c17d9402
HJ <kgj6200@naver.com>
2020-10-13 20:34:39 +0900

    queue

--- src/baekJoon/bj_12.java
+++ src/baekJoon/bj_12.java
@@ -12,21 +12,21 @@ public class bj_12 {
 		// N장의 카드 입력받기
 		int n = scanner.nextInt(); 
 		//FIFO인 Queue사용, Queue의 인터페이스 구현체인 LinkedList 사용
-		Queue queue = new LinkedList();
-		for(int i=0; i<n-1; i++) {
-			queue.offer(i+1);
+		Queue<Integer> queue = new LinkedList<Integer>();
+		for(int i=1; i<=n; i++) {
+			queue.offer(i);
 		}
 		//[0]을 버리고 [1]을 맨 밑으로 [2]를 버린다. [3]을 맨 밑으로
 		//card 한 개 남을 때 까지
 		//버린 카드들 순서대로 출력->남은 card 출력
 		while(!queue.isEmpty()) {
-			Object one = queue.poll(); //홀수 값 버림
-			System.out.println(one+" "); //출력
+			int one = queue.poll(); //홀수 값 버림
+			System.out.print(one+" "); //출력
 			
 			if(queue.isEmpty()) { // 값이 없으면 멈춤
 				break;
 			}
-			Object two = queue.poll(); //짝수 값 꺼냄
+			int two = queue.poll(); //짝수 값 꺼냄
 			queue.offer(two); //queue에 다시 저장
 		}
 	}
