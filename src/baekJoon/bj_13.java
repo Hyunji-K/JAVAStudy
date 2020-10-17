6676c3de73ebe8d81cff4f0e96eadfa031747dde
HJ <kgj6200@naver.com>
2020-10-18 00:00:55 +0900

    1065

---
+++ src/baekJoon/bj_13.java
@@ -0,0 +1,14 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj_13 {
+
+	public static void main(String[] args) {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
